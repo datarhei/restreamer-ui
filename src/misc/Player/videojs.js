25076eeb652fb7328cbb2ac717cc564583a121c9
r3rex <18644035+BlinkStrike@users.noreply.github.com>
2025-10-03 01:20:07 +0400

    try changing video player

--- src/misc/Player/videojs.js
+++ src/misc/Player/videojs.js
@@ -58,11 +58,11 @@ export default function VideoJS(props) {
 			direction="column"
 			justifyContent="center"
 			alignItems="center"
-			spacing={2}
-			style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}
+			wrap="nowrap"
+			style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, margin: 0 }}
 		>
-			<div data-vjs-player>
-				<video ref={videoRef} controls playsInline />
+			<div data-vjs-player style={{ width: '100%', height: '100%' }}>
+				<video ref={videoRef} controls playsInline style={{ width: '100%', height: '100%' }} />
 			</div>
 		</Grid>
 	);
